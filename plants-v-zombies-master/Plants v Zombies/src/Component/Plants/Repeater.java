f8218f660bfadd6c3a6c2aa90c2ac6973b3e9ee7
Mohammad Rezaei <rezaeim748@gmail.com>
2021-02-03 17:52:38 +0330

    Gui done

---
+++ plants-v-zombies-master/Plants v Zombies/src/Component/Plants/Repeater.java
@@ -0,0 +1,63 @@
+package Component.Plants;
+
+import Component.Bullets.Pea;
+import MusicPlayer.AudioPlayer;
+import Template.GameState;
+
+import javax.swing.*;
+import java.util.TimerTask;
+
+/**
+ * This class represents a repeater
+ *
+ * @author Feij
+ */
+public class Repeater extends Plant {
+
+    /**
+     * Perform any initialization that is required
+     * @param locX The pea shooter x coordinate
+     * @param locY The pea shooter y coordinate
+     * @param state The game state
+     */
+    public Repeater(int locX, int locY, GameState state){
+        super(locX, locY, 100, state);
+        setCurrentImage(new ImageIcon("./Pics/repeater.gif").getImage());
+        getTimer().schedule(new Shooter(), 0, 500);
+    }
+
+    /**
+     * Shoot pea
+     */
+    private void shoot(){
+        Pea pea = new Pea(getLocX() + 50, getLocY(), getState());
+        getState().addBullet(pea);
+        if(!getState().isMute()){
+            AudioPlayer shootSound = new AudioPlayer("./Sounds/shoot.wav", 0);
+        }
+    }
+
+    /**
+     * A class to handle shooting process
+     */
+    private class Shooter extends TimerTask {
+
+        /**
+         * The action to be performed by this timer task.
+         */
+        @Override
+        public void run() {
+            if(getState().isZombieInWay(getLocY(), getLocX()))
+                shoot();
+            setLoadTime(0);
+            setTimeHolder(System.currentTimeMillis());
+        }
+    }
+
+    public void load(){
+        super.load();
+        setCurrentImage(new ImageIcon("./Pics/repeater.gif").getImage());
+        setTask(new Shooter());
+        getTimer().schedule(getTask(), 500 - getLoadTime() ,500);
+    }
+}
---
+++ plants-v-zombies-master/Plants v Zombies/src/Gui/ScoreBoard.java
@@ -0,0 +1,63 @@
+package Gui;
+
+import Network.Client;
+import Network.User;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class ScoreBoard extends JFrame {
+
+    private JTable scoreboard;
+    private DefaultTableModel tableModel;
+    private JScrollPane scrollPane;
+    private Client client;
+
+    public ScoreBoard(Client client){
+        super("Top Players");
+        this.setSize(new Dimension(500, 500));
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        this.client = client;
+
+        tableModel = new DefaultTableModel();
+        tableModel.addColumn("Rank");
+        tableModel.addColumn("Username");
+        tableModel.addColumn("Score");
+        tableModel.addColumn("Normal Wins/Hard Wins");
+        tableModel.addColumn("Normal Loses/Hard Loses");
+        scoreboard = new JTable(tableModel);
+        scoreboard.setBackground(Color.cyan);
+        scoreboard.setOpaque(true);
+
+        scrollPane = new JScrollPane(scoreboard);
+        scrollPane.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+        scrollPane.createVerticalScrollBar();
+
+        client.connect("Get Users");
+        ArrayList<User> users = client.getUsers();
+        for(User user : users){
+            String[] information = new String[5];
+            information[0] = String.valueOf(users.indexOf(user) + 1);
+            information[1] = user.getUsername();
+            information[2] = String.valueOf(user.getScore());
+            information[3] = user.getNormalWins() + "/" + user.getHardWins();
+            information[4] = user.getNormalLoses() + "/" + user.getHardLoses();
+
+            tableModel.insertRow(tableModel.getRowCount(), information);
+        }
+
+        add(scrollPane);
+        setVisible(true);
+    }
+
+
+    public void dispose(){
+        setVisible(false);
+        MainMenuFrame mainMenuFrame = new MainMenuFrame(client);
+    }
+}
